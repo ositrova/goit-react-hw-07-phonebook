cf87d11e0c1b36d1aa8efe25e43ec8337e0fbfe2
ositrova <110326755+ositrova@users.noreply.github.com>
2023-04-10 13:51:16 +0300

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -6,7 +6,7 @@ export const Loader = () => (
       <RotatingTriangles
         visible={true}
         height="100"
-        width="100 "
+        width="100"
         ariaLabel="rotating-triangels-loading"
         wrapperStyle={{}}
         wrapperClass="rotating-triangels-wrapper"
